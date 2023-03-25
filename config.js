a7afab2ccb53a4d95a1ff2821c6367ca2d27d0b6
TMWP44 <74853607+TMWP44@users.noreply.github.com>
2023-03-25 09:50:07 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBUeZiqFvDvhltHy7QxUG1Jpah2DgR_8ow",
+  authDomain: "project67-f05ee.firebaseapp.com",
+  databaseURL: "https://project67-f05ee-default-rtdb.firebaseio.com",
+  projectId: "project67-f05ee",
+  storageBucket: "project67-f05ee.appspot.com",
+  messagingSenderId: "823444040854",
+  appId: "1:823444040854:web:a5f9f2bcba369c0e4c26b7"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
